fb51a103d56bbd5b172806385cedfc2af67d8df6
Luis Marroquin <luisfer.marroquin@hotmail.com>
2022-02-27 22:50:49 -0600

    Implementation of contracts APIs.

--- src/app.js
+++ src/app.js
@@ -1,21 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const {Op} = require('sequelize');
+const {sequelize} = require('./model');
+const {getProfile} = require('./middleware/getProfile');
 const app = express();
 app.use(bodyParser.json());
-app.set('sequelize', sequelize)
-app.set('models', sequelize.models)
+app.set('sequelize', sequelize);
+app.set('models', sequelize.models);
+const contractsRouter = express.Router();
+const jobsRouter = express.Router();
 
 /**
  * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
+contractsRouter.get('/:id', getProfile ,async (req, res) =>{
+    const {Contract} = req.app.get('models');
+    const {id} = req.params;
+    const contract = await Contract.findOne({where: {id}});
+    if(contract.ContractorId !== req.profile.id && contract.ClientId !== req.profile.id) return res.status(403).json({message: 'You do not have access to the requested contract'});
+    if(!contract) return res.status(404).end();
+    res.json(contract);
 })
+
+contractsRouter.get('/', getProfile, async (req, res) => {
+    const {Contract} = req.app.get('models');
+    const filter = {
+        [Op.not]: {status: 'terminated'},
+        [Op.or]: {ContractorId: req.profile.id, ClientId: req.profile.id}
+    };
+    const contracts = await Contract.findAll({where: filter})
+    res.status(200).json(contracts);
+})
+
+
+
+
+app.use('/contracts', contractsRouter);
+app.use('/jobs', jobsRouter);
 module.exports = app;
